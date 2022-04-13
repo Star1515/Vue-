46764a50331ba76c9b50676c2f696a5bc6fdf79d
Star1515 <85513028+Star1515@users.noreply.github.com>
2022-04-13 15:07:10 +0300

    Add files via upload

---
+++ src/components/PaymentDisplay.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="list">
+    <div class="item" v-for="item in list" :key="item.id">
+      {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaymentsDisplay",
+  props: {
+    list: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style>
+</style>
